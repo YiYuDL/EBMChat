b238f0b3c074d2ed7f2e15341145cb06260dc05e
Yi Yu <58670828+YiYuDL@users.noreply.github.com>
2025-10-28 09:19:59 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ebmchat',
+    version='1.1.0',
+    packages=['EBMChat'],
+    url='https://github.com/YiYuDL/EBMChat'
+    author='Yi Yu',
+    packages=find_packages(),
+    author_email='yuyi689@gmail.com',
+    install_requires=[
+       
+    ],
+)
